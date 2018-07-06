a13b8e72515bf2ea9e8bdc82f28fe09f838eefea
Naoki Ohzeki <0h23k1.n40k1@gmail.com>
2018-07-06 11:35:18 +0900

    feat: change visualizer style

--- js/index.js
+++ js/index.js
@@ -56,25 +56,81 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
   const sourceNode = audioContext.createMediaStreamSource(stream);
   const analyserNode = audioContext.createAnalyser();
   analyserNode.fftSize = 2048;
+  analyserNode.minDecibels = -60;  // Default -100 dB
+  analyserNode.maxDecibels = -0;  // Default  -30 dB
+
+  var paddingTop    = 20;
+  var paddingBottom = 20;
+  var paddingLeft   = 30;
+  var paddingRight  = 30;
+
+  var iw  = cw  - paddingLeft - paddingRight;
+  var ih = ch - paddingTop - paddingBottom;
+  var ib = ch - paddingBottom;
+
+  const range = analyserNode.maxDecibels - analyserNode.minDecibels;
+
+  // Frequency resolution
+  const fsDivN = audioContext.sampleRate / analyserNode.fftSize;
+
+   // This value is the number of samples during 500 Hz
+   const n500Hz = Math.floor(500 / fsDivN);
+
   sourceNode.connect(analyserNode);
 
   function draw() {
-    const array = new Uint8Array(analyserNode.fftSize);
-    analyserNode.getByteTimeDomainData(array);
-    const barWidth = cw / analyserNode.fftSize;
+    const array = new Float32Array(analyserNode.frequencyBinCount / 8);
+    analyserNode.getFloatFrequencyData(array);
+    const barWidth = cw / analyserNode.frequencyBinCount;
+
+    // init
     drawContext.fillStyle = 'rgba(0, 0, 0, 1)';
     drawContext.fillRect(0, 0, cw, ch);
 
-    for (let i = 0; i < analyserNode.fftSize; ++i) {
-      const value = array[i];
-      const percent = value / 255;
-      const height = ch * percent;
-      const offset = ch - height;
+    drawContext.beginPath();
 
-      drawContext.fillStyle = 'lime';
-      drawContext.fillRect(i * barWidth, offset, barWidth, 2);
+    for (let i = 0, len = array.length; i < len; ++i) {
+      const x = Math.floor((i / len) * iw) + paddingLeft;
+      const y = Math.floor(-1 * ((array[i] - analyserNode.maxDecibels) / range) * ih) + paddingTop;
+
+      if (i === 0) {
+        drawContext.moveTo(x, y);
+      } else {
+        drawContext.lineTo(x, y);
+      }
+
+      if (i % n500Hz === 0) {
+        const text = (500 * (i / n500Hz) / 1000) + 'kHz';  // index -> frequency
+
+        // Draw grid (X)
+        drawContext.fillStyle = 'rgba(255, 0, 0, 1.0)';
+        drawContext.fillRect(x, paddingTop, 1, ih);
+
+        // Draw text (X)
+        drawContext.fillStyle = 'rgba(255, 255, 255, 1.0)';
+        drawContext.font      = '12px "Times New Roman"';
+        drawContext.fillText(text, (x - (drawContext.measureText(text).width / 2)), (ch - 3));
+      }
     }
 
+    drawContext.strokeStyle = 'rgba(0, 0, 255, 1.0)';
+    drawContext.lineWidth   = 2;
+    drawContext.lineCap     = 'round';
+    drawContext.lineJoin    = 'miter';
+    drawContext.stroke();
+
+    for (let i = analyserNode.minDecibels; i <= analyserNode.maxDecibels; i += 10) {
+      const gy = Math.floor(-1 * ((i - analyserNode.maxDecibels) / range) * ih) + paddingTop;
+
+      // Draw grid (Y)
+      drawContext.fillStyle = 'rgba(255, 0, 0, 1.0)';
+      drawContext.fillRect(paddingLeft, gy, iw, 1);
+
+      // Draw text (Y)
+      drawContext.fillStyle = 'rgba(255, 255, 255, 1.0)';
+      drawContext.font      = '10px "Times New Roman"';
+      drawContext.fillText((i + ' dB'), 3, gy);
+    }
     requestAnimationFrame(draw);
   }
 
